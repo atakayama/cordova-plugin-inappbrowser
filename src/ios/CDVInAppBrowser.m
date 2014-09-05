479268903fe3e6de90abb5c2d8879fff7ee77ecb
Atsushi Takayama <a.takayama@infocom.co.jp>
2014-09-05 22:34:22 +0900

    _self以外はシステムブラウザで開くように変更

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -97,10 +97,8 @@
 
         if ([target isEqualToString:kInAppBrowserTargetSelf]) {
             [self openInCordovaWebView:absoluteUrl withOptions:options];
-        } else if ([target caseInsensitiveCompare:kInAppBrowserTargetSystem] == NSOrderedSame) {
-            [self openInSystem:absoluteUrl];
-        } else { // _blank or anything else
-            [self openInInAppBrowser:absoluteUrl withOptions:options];
+        } else { // anything but _self
+            [self openInSystem:absoluteUrl withOptions:options];
         }
 
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
