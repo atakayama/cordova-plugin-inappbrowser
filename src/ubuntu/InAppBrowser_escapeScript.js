2612c3e94cc7f10867a0fbacc87551179661507b
ymanya <y.manya@infocom.co.jp>
2015-05-28 15:26:15 +0900

    オリジナルのinappbrowserをv1.0.0にアップデート

--- src/ubuntu/InAppBrowser_escapeScript.js
+++ src/ubuntu/InAppBrowser_escapeScript.js
@@ -1,4 +1,5 @@
 /*
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -15,7 +16,14 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
 */
-#style-update-file {
-    display: block !important;
-}
+
+oxide.addMessageHandler("EXECUTE", function(msg) {
+    var code = msg.args.code;
+    try {
+        msg.reply({result: eval(code)});
+    } catch(e) {
+        msg.error("Code threw exception: \"" + e + "\"");
+    }
+});
